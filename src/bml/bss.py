1517a84d4dc1c97c144988d692c7558b08bfaf70
Gert-Jan <gert-jan.paulissen@gmail.com>
2018-01-18 13:35:37 +0100

    Expanding variables improved
    - including bidding history
    - variables in description solved

--- src/bml/bss.py
+++ src/bml/bss.py
@@ -118,7 +118,7 @@ class Sequence:
 def systemdata_normal(child):
     return child.bid_type()['normal'] == True
 
-def systemdata_bidtable_old(children, systemdata, vars={}):
+def systemdata_bidtable(children, systemdata, vars={}):
     bids_processed = []
 
     for c in children:
@@ -185,14 +185,24 @@ def systemdata_bidtable_old(children, systemdata, vars={}):
                             vars[var] = k
                         h = copy.deepcopy(c)
                         h.bid = bid
-                        systemdata_bidtable_old([h], systemdata, vars)
+                        if var:
+                            # Replace the variable by its definition.
+                            # So "1M" and "1 M" must be replaced but be aware
+                            # that the first is a Python word.
+                            s = r'([0-9]+)' + var + r'\b'
+                            d = '\1' + vars[var]
+                            h.desc = re.sub(s, d, h.desc)
+                            s = r'\b' + var + r'\b'
+                            d = vars[var]
+                            h.desc = re.sub(s, d, h.desc)
+                        systemdata_bidtable([h], systemdata, vars)
                         if add_var:
                             vars.pop(var, None) # remove M
                         bids_processed.append(bid)
             else:
                 assert c.all_bids()[-1] == bml.EMPTY, 'Bid (%s) must be empty' % (c.bid)
                 assert len(children) == 1
-                systemdata_bidtable_old(c.children, systemdata, vars) # the function will stop after this call since there are no other children
+                systemdata_bidtable(c.children, systemdata, vars) # the function will stop after this call since there are no other children
         else: # systemdata_normal(c):
             if bml.args.verbose > 1:
                 print("Normal child: %s" % (c))
@@ -209,114 +219,60 @@ def systemdata_bidtable_old(children, systemdata, vars={}):
             if bml.args.verbose > 1:
                 print("Seq: %s" % (seq))
 
-            systemdata_bidtable_old(c.children, systemdata, vars)
+            systemdata_bidtable(c.children, systemdata, vars)
             bids_processed.append(c.all_bids()[-1])
         
-def systemdata_bidtable_new(children, systemdata, vars={}):
-    bids_processed = []
-
-    for c in children:
-        all_normal = True
-        for i in range(len(c.all_bids())):
-            if not c.bid_type(i)['normal']:
-                if bml.args.verbose > 1:
-                    print("Special child: %s" % (c))
-
-                all_normal = False
-
-                var = None # A strain variable 
-                strain = None
-
-                # special bids of the form <digit><strain>
-                # for instance 1HS, 2M, 3m etc
-                bid = c.bid_type(i)
-                if bid and bid['level']:
-                    level = bid['level']
-                    strain = bid['strain']
-                    if re.match(r'[CDHS]+\Z', strain):
-                        pass
-                    elif strain == 'M':
-                        var = strain
-                        strain = 'HS'
-                    elif strain == 'm':
-                        var = strain
-                        strain = 'CD'
-                    elif strain.upper() == 'BLACK':
-                        strain = 'CS'
-                    elif strain.upper() == 'RED':
-                        strain = 'DH'
-                    elif strain.upper() == 'X':
-                        var = 'X'
-                        strain = 'CDHS'
-                    elif strain.upper() in ['STEP', 'STEPS']:
-                        parentbid = Bid(c.parent.all_bids()[-1])
-                        parentbid += int(level)
-                        m = re.match('(?P<level>[1-7])(?P<strain>[CDHSN])\Z', str(parentbid))
-                        assert m != None
-                        level = m.group['level']
-                        strain = m.group['strain']
-                    elif strain == 'oM':
-                        assert 'M' in vars, 'When variable "oM" is used, variable "M" must already be defined'
-                        var = strain
-                        strain = 'S' if vars['M'] == 'H' else 'H'
-                    elif strain == 'om':
-                        assert 'm' in vars, 'When variable "om" is used, variable "m" must already be defined'
-                        var = strain
-                        strain = 'D' if vars['m'] == 'C' else 'C'
-                    else:
-                        raise Exception("Unknown strain (%s)" % (strain))
-
-                    # If the variable is already defined use that definition else add the variable (temporarily)
-                    add_var = False
-                    if var:
-                        if var in vars:
-                            strain = vars[var]
-                        else:
-                            add_var = True
-                            
-                    for k in strain:
-                        bid = level + k
-                        
-                        # We must not add such a new bid if that bid was already processed here before
-                        if bid not in bids_processed:
-                            if add_var:
-                                vars[var] = k
-                            h = copy.deepcopy(c)
-                            h.bid = bid
-                            systemdata_bidtable_new([h], systemdata, vars)
-                            if add_var:
-                                vars.pop(var, None) # remove M
-                            bids_processed.append(bid)
-                else:
-                    assert c.all_bids()[-1] == bml.EMPTY, 'Bid (%s) must be empty' % (c.bid)
-                    assert len(children) == 1
-                    systemdata_bidtable_new(c.children, systemdata, vars) # the function will stop after this call since there are no other children
-
-        if c.bid == bml.EMPTY or not(all_normal):
-            break
-
-        seq = Sequence(c)
-        if not seq in systemdata:
-            systemdata.append(seq)
-        else:
-            si = systemdata.index(seq)
-            seq = systemdata[si]
-            if not systemdata[si].desc:
-                systemdata[si].desc = c.desc
-
-        if bml.args.verbose > 1:
-            print("Seq: %s" % (seq))
-
-        systemdata_bidtable_new(c.children, systemdata, vars)
-        bids_processed.append(c.all_bids()[-1])
-
 def to_systemdata(content):
     systemdata = []
 
     for c in content.nodes:
         content_type, content = c
         if content_type == bml.ContentType.BIDTABLE:
-            systemdata_bidtable_old(content.children, systemdata)
+            assert content.bid == bml.ROOT, 'The first node must be root'
+            assert len(content.children) == 1
+            # Bidding history is the first child of root.
+            #
+            # The idea is to expand the bidding history so
+            # systemdata_bidtable() can also define the variables in the history.
+            #
+            # We do this by creating new nodes after the first bid in the history.
+            #
+            # For example we start with:
+            #
+            # history.children = C
+            # history.desc = D
+            # history.bid = "b1-b2"
+            #
+            # After expanding we get:
+            #
+            # history.children = [node]
+            # history.desc = ''
+            # history.bid = "b1"
+            #
+            # node.bid = "b2"
+            # node.desc = D
+            # node.children = C
+            
+            history = content.children[0]
+            bids = history.all_bids()
+            if len(bids) > 1:
+                indentation = bml.args.indentation
+                children = history.children
+                desc = history.desc
+                history.children = []
+                history.desc = ''
+                history.bid = bids[0]
+                parent = history
+                for bid in bids[1:]:
+                    child = parent.add_child(bid=bid, desc='', indentation=indentation, desc_indentation=-1)
+                    indentation = indentation + bml.args.indentation
+                    assert child.parent == parent
+                    assert len(child.children) == 0
+                    parent = child
+                else: # at the end of the loop
+                    parent.set_children(children)
+                    parent.desc = desc
+            systemdata_bidtable(content.children, systemdata)
 
     return systemdata
 
